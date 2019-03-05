a8a4f353e8b4c1d2b78daae871279cc727636d91
Ben Dudley <bdudley@e2r6p1.21-school.ru>
2019-03-05 22:13:19 +0300

    check_tetrimino with 1 problem(map[1] shifts ine string but figure

--- solution.c
+++ solution.c
@@ -6,7 +6,7 @@
 /*   By: bdudley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 19:20:14 by bdudley           #+#    #+#             */
-/*   Updated: 2019/03/05 19:13:50 by bdudley          ###   ########.fr       */
+/*   Updated: 2019/03/05 22:11:01 by bdudley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,49 @@
 #include "stdio.h"
 
 //Функция чекающая можем мы вставить фигуру или нет
-int		check_tetrimino()
+int		check_tetrimino(t_tetriminos tetrimino, unsigned short size_map, unsigned short map[17])
 {
+	unsigned short row;
+
+	row = 0;
+	while (tetrimino.x + tetrimino.width > size_map)
+	{
+		tetrimino.y += 1;	
+		if (tetrimino.y + tetrimino.height > size_map)
+			return (0);
+	}
+	while (row < 4)
+	{
+		while (((tetrimino.value << 4*row)*(tetrimino.value >> (3 - row) * 4) >> tetrimino.x) & map[tetrimino.y + row])
+		{
+			tetrimino.x++;
+			if (tetrimino.x + tetrimino.width > size_map)
+				tetrimino.y += 1;	
+			if (tetrimino.y + tetrimino.height > size_map)
+				return (0);
+		}
+		row++;
+	}
 	return (1);
 }
 
-int	solution(t_tetriminos tetriminos[27], unsigned short size_map, unsigned short map[17])
+int	solution(t_tetriminos tetriminos[27], unsigned short size_map, unsigned short map[17], unsigned short number)
 {
-	static int number = 0;
 	if (tetriminos->letter == '\0')
 		return (1);
-	if (tetriminos->x + tetriminos->width >= size_map)
-		tetriminos->y += 1;	
-	if (tetriminos->y + tetriminos->height > size_map)
-		return (0);	
 	//В зависимости от текущего размера карты проверить сможем ли мы поставить фигуру
-	if (check_tetrimino())
+	//map[0]= 32768;
+	if (check_tetrimino(tetriminos[number], size_map, map))
 	{
+	printf("READY!");
 		//1, тогда перейти к следующей фигуре
 		//записать в мап найденную фигуру
-		solution(tetriminos + 1, size_map, map);
+//		solution(tetriminos, size_map, &(*map), number + 1);
 		return (1);
 	}
 	else
 	{
+	printf("False!");
 		//0, тогда откатится к предыдущей фигуре, почистить поле и переместить предыдущую фигуру на другую позицию, а у этой фигкрк обнулить все координаты
 
 	}
@@ -51,11 +70,14 @@ void		solution_help(t_tetriminos tetriminos[27])
 
 	size_map = 2;
 	ft_bzero(map, 17*sizeof(unsigned short));
+	/*int i;
+	i = -1;
+	while (++i < 18)
+		printf("map[%d] =%d\n",i, map[i]);*/
 //определяющий минимальный размер карты
-	while ((solution(tetriminos, size_map, map)) == 0)
+	while ((solution(tetriminos, size_map, map, 0)) == 0)
 	{
 		ft_bzero(map, 17*sizeof(unsigned short));
 		size_map++;
 	}
-	printf("READY!");
 }
