0817845e6aa73a31fdbeb21458ff1ee12e232457
nazym <nazymanteater@gmail.com>
2019-03-06 01:58:39 +0300

    finished functions in init.c

--- init.c
+++ init.c
@@ -1,16 +1,62 @@
 #include "fillit.h"
 
-unsigned short	get_width(const char *str)
+unsigned short	get_width(unsigned short value)
 {
+	unsigned short width;
+        int             i;
+
+        width = 1;
+        i = 0;
+
+        while (i < 3)
+        {
+                if (value & (0b100010001000100 >> (1 * i)))
+                        width += 1;
+                else
+                        break ;
+                i += 1;
+        }
+        return (width);
 }
 
-unsigned short  get_height(const char *str)
+unsigned short  get_height(unsigned short value)
 {
+	unsigned short height;
+	int		i;
+
+	height = 1;
+	i = 0;
+
+	while (i < 3)
+	{
+		if (value & (0b111100000000 >> (4 * i)))
+			height += 1;
+		else
+			break ;
+		i += 1;
+	}
+	return (height);
 }
 
 unsigned short  get_value(const char *str)
 {
+	unsigned short value;
 
+	value = 0;
+	while (*str) {
+        	if (*str == '#' || *str == '.')
+                {
+                        value = value << 1;
+                        if (*str == '#')
+                                value = value | 1;
+                }
+                str += 1;
+        }
+	while (!(value & 0b1000100010001000))
+		value = value << 1;
+	while (!(value & 0b1111000000000000))
+		value = value << 4;
+	return (value);
 }
 
 t_tetriminos     init_tetriminos(const char *str, char letter)
@@ -18,9 +64,8 @@ t_tetriminos     init_tetriminos(const char *str, char letter)
         t_teriminos      tetriminos;
 
         tetriminos.letter = letter;
-        tetriminos.width = get_width(str);
-        tetriminos.height = get_height(str);
         tetriminos.value = get_value(str);
+	tetriminos.width = get_width(tetriminos.value);
+        tetriminos.height = get_height(tetriminos.value);
         return (tetriminos);
 }
-
