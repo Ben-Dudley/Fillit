6f4831873516b40a5d1beacfa51f2da6b914a380
Ben Dudley <bdudley@e2r6p1.21-school.ru>
2019-03-09 13:52:31 +0300

    norminette solution.c

--- solution.c
+++ solution.c
@@ -6,29 +6,31 @@
 /*   By: bdudley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 19:20:14 by bdudley           #+#    #+#             */
-/*   Updated: 2019/03/09 13:21:39 by bdudley          ###   ########.fr       */
+/*   Updated: 2019/03/09 13:51:53 by bdudley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "stdio.h"
 
-int		check_tetrimino(t_tetriminos *tetrimino, unsigned short size_map, unsigned short map[17])
+int				check_tetrimino(t_tetriminos *tetrimino,
+		unsigned short size_map, unsigned short map[17])
 {
-	short row;
-	unsigned short tmp;
+	short			row;
+	unsigned short	tmp;
 
 	row = -1;
 	while (++row < 4)
 	{
-		while ((((*tetrimino).x + (*tetrimino).width) > size_map) && (((*tetrimino).y + (*tetrimino).height) <= size_map))
+		while ((((*tetrimino).x + (*tetrimino).width) > size_map)
+				&& (((*tetrimino).y + (*tetrimino).height) <= size_map))
 		{
-			(*tetrimino).y += 1;	
+			(*tetrimino).y += 1;
 			(*tetrimino).x = 0;
 		}
 		if ((*tetrimino).y + (*tetrimino).height > size_map)
 			return (0);
-		tmp =((*tetrimino).value << 4*row);
+		tmp = ((*tetrimino).value << 4 * row);
 		tmp = tmp >> 12;
 		tmp = (tmp << 12) >> (*tetrimino).x;
 		printf("index = %d", (*tetrimino).y);
@@ -41,40 +43,41 @@ int		check_tetrimino(t_tetriminos *tetrimino, unsigned short size_map, unsigned 
 	return (1);
 }
 
-void	put_shape(t_tetriminos tetrimino, unsigned short *map[17])
+void			put_shape(t_tetriminos tetrimino, unsigned short *map[17])
 {
-	short row;
-	short num;
-	unsigned short tmp;
+	short			row;
+	short			num;
+	unsigned short	tmp;
 
 	row = -1;
 	while (++row < 4)
 	{
-		tmp =(tetrimino.value << 4*row);
+		tmp = (tetrimino.value << 4 * row);
 		tmp = tmp >> 12;
 		tmp = (tmp << 12) >> tetrimino.x;
 		(*map)[tetrimino.y + row] = (*map)[tetrimino.y + row] + tmp;
-		printf("put = %d\n",(*map)[tetrimino.y +  row]);
+		printf("put = %d\n", (*map)[tetrimino.y + row]);
 	}
 }
 
-void	delete_shape(t_tetriminos tetrimino, unsigned short *map[17])
+void			delete_shape(t_tetriminos tetrimino, unsigned short *map[17])
 {
-	short row;
-	short num;
-	unsigned short tmp;
+	short			row;
+	short			num;
+	unsigned short	tmp;
 
 	row = -1;
 	while (++row < 4)
 	{
-		tmp =(tetrimino.value << 4*row);
+		tmp = (tetrimino.value << 4 * row);
 		tmp = tmp >> 12;
 		tmp = (tmp << 12) >> tetrimino.x;
 		(*map)[tetrimino.y + row] = (*map)[tetrimino.y + row] - tmp;
 	}
 }
 
-int	solution(t_tetriminos tetriminos[27], unsigned short size_map, unsigned short map[17], int  number)
+int				solution(t_tetriminos tetriminos[27], unsigned short size_map,
+		unsigned short map[17], int number)
 {
 	printf("Start! %d\n", size_map);
 	if (tetriminos[number].letter == '\0')
@@ -96,7 +99,7 @@ int	solution(t_tetriminos tetriminos[27], unsigned short size_map, unsigned shor
 		if (number - 1 >= 0)
 		{
 			printf("delete_shape");
-			delete_shape(tetriminos[number -1], &map);
+			delete_shape(tetriminos[number - 1], &map);
 			print(tetriminos, size_map, number + 1);
 			tetriminos[number - 1].x++;
 			solution(tetriminos, size_map, map, number - 1);
@@ -104,21 +107,21 @@ int	solution(t_tetriminos tetriminos[27], unsigned short size_map, unsigned shor
 		else
 			return (0);
 	}
-	return(0);
+	return (0);
 }
 
-unsigned short		find_smallest_square(t_tetriminos tetriminos[27], int number)
+unsigned short	find_smallest_square(t_tetriminos tetriminos[27], int number)
 {
-	unsigned short map[17];
+	unsigned short	map[17];
 	unsigned short	size_map;
 
 	size_map = 2;
-	while (size_map*size_map < number*4)
+	while (size_map * size_map < number * 4)
 		size_map++;
-	ft_bzero(map, 17*sizeof(unsigned short));
+	ft_bzero(map, 17 * sizeof(unsigned short));
 	while ((solution(tetriminos, size_map, map, 0)) == 0)
 	{
-		ft_bzero(map, 17*sizeof(unsigned short));
+		ft_bzero(map, 17 * sizeof(unsigned short));
 		size_map++;
 	}
 	return (size_map);
